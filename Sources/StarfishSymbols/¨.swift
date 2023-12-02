df9ca5fa9109f781d6b06f5e32650e8c36ccc793
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2023-12-02 12:52:18 -0600

    compactMap operator

--- Sources/StarfishSymbols/¨.swift
+++ Sources/StarfishSymbols/¨.swift
@@ -10,8 +10,26 @@ import Foundation
 import Starfish
 
 infix operator →
+infix operator ⇢
 
 public func →<X, Y>(_ xs: [X], _ f: (X) throws -> Y) throws -> [Y]
 {
     return try xs.map(f)
+}
+
+public func ⇢<X, Y>(_ xs: [X], _ f: (X) throws -> Y) -> [Y]
+{
+    return xs.compactMap
+    {
+        x in
+
+        do
+        {
+            return try f(x)
+        }
+        catch
+        {
+            return nil
+        }
+    }
 }
