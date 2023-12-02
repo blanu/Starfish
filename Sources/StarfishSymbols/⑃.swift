4199b398bcb03b038659f9d8879f1ab354b00524
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2023-12-02 11:01:11 -0600

    fork

---
+++ Sources/StarfishSymbols/⑃.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Dr. Brandon Wiley on 12/2/23.
+//
+
+import Foundation
+import Starfish
+
+prefix operator ⋔
+
+public prefix func ⋔<X, Y, Z>(_ tuple: ((Y, Y) -> Z, (X) -> Y, (X) -> Y)) -> (X) -> Z
+{
+    let (f, g, h) = tuple
+    return fork(f, g, h)
+}
